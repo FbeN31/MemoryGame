1c47795e3d104f4207f303b167f6119c4f375bb8
FbeN31 <frolytheking@gmail.com>
2024-05-17 12:07:44 +0300

    xml fixed

--- app/src/main/java/com/example/memorygame/MainActivity.java
+++ app/src/main/java/com/example/memorygame/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.memorygame;
 import android.content.Intent;
 import android.graphics.drawable.shapes.Shape;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -61,27 +62,42 @@ public class MainActivity extends AppCompatActivity {
             cardView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (isBusy) {
+                    Log.e("XXX", "line 65");
+                    if (isBusy)
+                    {
+                        Log.e("XXX", "line 68");
+
                         return;
                     }
 
                     ImageView card = (ImageView) v;
                     int index = getCardIndex(card);
 
-                    if (card.getDrawable() != null) {
+                    if (card.getDrawable() == null)
+                    {
+                        Log.e("XXX", "line 78");
                         return;
                     }
 
-                    if (flippedCardsCount == 0) {
+                    if (flippedCardsCount == 0)
+                    {
+                        Log.e("XXX", "line 84");
                         firstFlippedCard = card;
                         showCard(firstFlippedCard, index);
-                    } else if (flippedCardsCount == 1) {
+                    }
+                    else if (flippedCardsCount == 1)
+                    {
+                        Log.e("XXX", "line 68");
                         secondFlippedCard = card;
                         showCard(secondFlippedCard, index);
 
-                        if (getCardImage(firstFlippedCard) == getCardImage(secondFlippedCard)) {
+                        if (getCardImage(firstFlippedCard) == getCardImage(secondFlippedCard))
+                        {
+                            Log.e("XXX", "line 69");
                             removeCards(firstFlippedCard, secondFlippedCard);
-                        } else {
+                        } else
+                        {
+                            Log.e("XXX", "line 70");
                             hideCards(firstFlippedCard, secondFlippedCard);
                         }
                     }
@@ -90,8 +106,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void shuffleCards() {
-        for (int i = 0; i < cardImages.length; i++) {
+    private void shuffleCards()
+    {
+        for (int i = 0; i < cardImages.length; i++)
+        {
+            Log.e("XXX", "line 71");
             int randomIndex = (int) (Math.random() * cardImages.length);
             int temp = cardImages[i];
             cardImages[i] = cardImages[randomIndex];
@@ -99,38 +118,58 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private int getCardIndex(ImageView card) {
-        for (int i = 0; i < cardViews.length; i++) {
-            if (cardViews[i] == card) {
+    private int getCardIndex(ImageView card)
+    {
+        for (int i = 0; i < cardViews.length; i++)
+        {
+            if (cardViews[i] == card)
+            {
+                Log.e("XXX", "line 72");
                 return i;
             }
         }
         return -1;
     }
 
-    private int getCardImage(ImageView card) {
+    private int getCardImage(ImageView card)
+    {
+        Log.e("XXX", "line 73");
         return cardImages[getCardIndex(card) % (cardImages.length / 2)];
     }
 
-    private void showCard(ImageView card, int index) {
+    private void showCard(ImageView card, int index)
+    {
+        Log.e("XXX", "line 74");
         card.setImageResource(cardImages[index % (cardImages.length / 2)]);
         flippedCardsCount++;
     }
 
-    private void hideCards(ImageView firstCard, ImageView secondCard) {
+    private void hideCards(ImageView firstCard, ImageView secondCard)
+    {
         isBusy = true;
-        new Thread(new Runnable() {
+        new Thread(new Runnable()
+        {
             @Override
-            public void run() {
-                try {
+            public void run()
+            {
+                Log.e("XXX", "line 75");
+                try
+                {
+                    Log.e("XXX", "line 76");
                     Thread.sleep(1000);
-                } catch (InterruptedException e) {
+                }
+                catch (InterruptedException e)
+                {
+                    Log.e("XXX", "line 77");
                     e.printStackTrace();
                 }
 
-                runOnUiThread(new Runnable() {
+                runOnUiThread(new Runnable()
+                {
                     @Override
-                    public void run() {
+                    public void run()
+                    {
+                        Log.e("XXX", "line 78");
                         firstCard.setImageResource(R.drawable.back_card);
                         secondCard.setImageResource(R.drawable.back_card);
                         flippedCardsCount = 0;
@@ -143,6 +182,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void removeCards(ImageView firstCard, ImageView secondCard)
     {
+        Log.e("XXX", "line 79");
         firstCard.setVisibility(View.INVISIBLE);
         secondCard.setVisibility(View.INVISIBLE);
         flippedCardsCount = 0;
@@ -156,13 +196,17 @@ public class MainActivity extends AppCompatActivity {
         {
             if (cardView.getVisibility() == View.VISIBLE)
             {
+                Log.e("XXX", "line 80");
                 gameOver = false;
                 break;
             }
         }
         if (gameOver)
         {
+            Log.e("XXX", "line 81");
             Toast.makeText(MainActivity.this, "Congratulations! You won the game!", Toast.LENGTH_LONG).show();
         }
     }
+
+
 }

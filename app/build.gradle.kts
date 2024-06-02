944f63d9f37a7bc5b6231203323508aac340a24a
FbeN31 <frolytheking@gmail.com>
2024-06-02 14:37:58 +0300

    finished game with login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:materLial:1.12.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:23.0.0")
     implementation("com.google.android.material:material:1.12.0")
